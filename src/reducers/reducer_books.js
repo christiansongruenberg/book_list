b997bf90949b67364fcd6860e45afd1be34188d4
christiansongruenberg <christianson.gruenberg@gmail.com>
2016-05-31 00:40:06 -0400

    half way through redux section

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Christianson on 5/30/2016.
+ */
+export default function(){
+    return [
+        {title: 'javascript: the Good parts'},
+        {title: 'harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquen Ruby'}
+    ]
+};
